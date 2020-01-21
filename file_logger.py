a9039a1830a8ffc1562a9e71ec73c7a463119c2b
Marcin Żyżyński <mzyzynsk@mion.elka.pw.edu.pl>
2020-01-21 19:41:26 +0100

    ulepszenie zapisu do pliku

--- file_logger.py
+++ file_logger.py
@@ -8,20 +8,27 @@ class FileLogger:
     def log_K(self, k):
         self.k = k
         self.file.write("K wynosi {}\n".format(k))
+        self.file.flush()
 
     def log_max_list(self, max_list):
         for index, max_value in enumerate(max_list):
             self.file.write("Po {} wywolaniach najlepsza wartosc wynosi: {}\n".format(pow(10, index) * self.k, max_value))
+        self.file.flush()
 
     def log_result(self, result):
         self.file.write(' '.join(map(str, result)) + "\n")
+        self.file.flush()
 
     def log_start_point(self, start_point):
         self.file.write(str(start_point))
         self.file.write("\n")
+        self.file.flush()
 
     def close(self):
         self.file.close()
+
+    def __del__(self):
+        self.close()
 
 
 if __name__ == '__main__':
